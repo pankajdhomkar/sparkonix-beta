85fd8be8a043f64d4d945a39a9b08a2d3bb998b0
Devp8 <Devp8@Devp8>
2017-08-03 11:22:32 +0530

    .

---
+++ src/main/resources/webapp/wareseller/addreseller/addnewresellerController.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('sparkonixWebApp').controller('addnewresellerController',
+		addnewresellerController);
+
+function addnewresellerController($scope, $state, restAPIService, dialogs,
+		$rootScope,$http) {
+	console.log("Here it come--");
+}
