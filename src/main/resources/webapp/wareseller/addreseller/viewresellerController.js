38732ea387b2d807810f0f10d35b83608a03fc5b
Devp8 <Devp8@Devp8>
2017-08-03 17:30:24 +0530

    .

---
+++ src/main/resources/webapp/wareseller/addreseller/viewresellerController.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('sparkonixWebApp').controller('viewresellerController',
+		viewresellerController);
+
+function viewresellerController($scope, $state, restAPIService, dialogs,
+		$rootScope,$http) {
+	console.log("Here it come--");
+	alert("Comes here in view reseller");
+}
